1a92f5f48284b7f32a3e92cceeebd5edb6faa789
NadimeJebali <NadimJebali.0@gmail.com>
2025-07-21 02:02:45 +0100

    trying to fetch from database

--- src/app/(dashboard)/list/teachers/page.tsx
+++ src/app/(dashboard)/list/teachers/page.tsx
@@ -5,18 +5,10 @@ import TableSearch from "@/components/TableSearch";
 import { role, teachersData } from "@/app/lib/data";
 import Image from "next/image";
 import Link from "next/link";
+import { Class, Subject, Teacher } from "@prisma/client";
+import prisma from "@/app/lib/prisma";
 
-type Teacher = {
-  id: number;
-  teacherId: string;
-  name: string;
-  email?: string;
-  photo: string;
-  phone: string;
-  subjects: string[];
-  classes: string[];
-  address: string;
-};
+type TeacherList = Teacher & {subjects:Subject[]} & {classes:Class[]}
 
 const columns = [
   {
@@ -54,44 +46,65 @@ const columns = [
   },
 ];
 
-const TeacherListPage = () => {
-  const renderRow = (item: Teacher) => (
-    <tr
-      key={item.id}
-      className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-[#F1F0FF]"
-    >
-      <td className="flex items-center gap-4 p-4 text-[#1F2937]">
-        <Image
-          src={item.photo}
-          alt=""
-          width={40}
-          height={40}
-          className="md:hidden xl:block w-10 h-10 rounded-full object-cover"
-        />
-        <div className="flex flex-col">
-          <h3 className="font-semibold">{item.name}</h3>
-          <p className="text-xs text-[#6B7280]">{item?.email}</p>
-        </div>
-      </td>
-      <td className="hidden md:table-cell text-[#1F2937]">{item.teacherId}</td>
-      <td className="hidden md:table-cell text-[#1F2937]">{item.subjects.join(", ")}</td>
-      <td className="hidden md:table-cell text-[#1F2937]">{item.classes.join(", ")}</td>
-      <td className="hidden lg:table-cell text-[#1F2937]">{item.phone}</td>
-      <td className="hidden lg:table-cell text-[#1F2937]">{item.address}</td>
-      <td>
-        <div className="flex items-center gap-2">
-          <Link href={`/list/teachers/${item.id}`}>
-            <button className="w-7 h-7 flex items-center justify-center hover:cursor-pointer rounded-full bg-[#C3EBFA]">
-              <Image src="/view.png" alt="" width={16} height={16} />
-            </button>
-          </Link>
-          {role === "admin" && (
-            <FormModal table="teacher" type="delete" id={item.id} />
-          )}
-        </div>
-      </td>
-    </tr>
-  );
+
+const renderRow = (item: TeacherList) => (
+  <tr
+    key={item.id}
+    className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-[#F1F0FF]"
+  >
+
+
+    <td className="flex items-center gap-4 p-4 text-[#1F2937]">
+      <Image
+        src={item.img || "/noAvatar.png"}
+        alt=""
+        width={40}
+        height={40}
+        className="md:block xl:block w-10 h-10 rounded-full object-cover"
+      />
+      <div className="flex flex-col">
+        <h3 className="font-semibold">{item.name}</h3>
+        <p className="text-xs text-[#6B7280]">{item?.email}</p>
+      </div>
+    </td>
+    <td className="hidden md:table-cell text-[#1F2937]">{item.username}</td>
+    <td className="hidden md:table-cell text-[#1F2937]">{item.subjects.map(subject=>subject.name).join(",")}</td>
+    <td className="hidden md:table-cell text-[#1F2937]">{item.classes.map(classItem=>classItem.name).join(",")}</td>
+    <td className="hidden lg:table-cell text-[#1F2937]">{item.phone}</td>
+    <td className="hidden lg:table-cell text-[#1F2937]">{item.address}</td>
+    <td>
+      <div className="flex items-center gap-2">
+        <Link href={`/list/teachers/${item.id}`}>
+          <button className="w-7 h-7 flex items-center justify-center hover:cursor-pointer rounded-full bg-[#C3EBFA]">
+            <Image src="/view.png" alt="" width={16} height={16} />
+          </button>
+        </Link>
+        {role === "admin" && (
+          <FormModal table="teacher" type="delete" id={item.id} />
+        )}
+      </div>
+    </td>
+  </tr>
+);
+
+const TeacherListPage = async ({searchParams}:{searchParams:{[key:string]:string | undefined};
+
+}) => {
+
+
+  const {page, ...queryParams}= searchParams;
+
+  const p = page ? parseInt(page) : 1;
+
+  const data = await prisma.teacher.findMany({
+    include:{
+      subjects:true,
+      classes:true,
+    },
+    take:10,
+    skip:10 * (p-1)
+  });
+
 
   return (
     <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
@@ -116,7 +129,7 @@ const TeacherListPage = () => {
         </div>
       </div>
       {/* LIST */}
-      <Table columns={columns} renderRow={renderRow} data={teachersData} />
+      <Table columns={columns} renderRow={renderRow} data={data} />
       {/* PAGINATION */}
       <Pagination />
     </div>
